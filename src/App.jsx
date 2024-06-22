c329a664041b5a877a42e5883c347b5eb12017b3
juanzb <juanzarache2000@gmail.com>
2024-06-22 11:50:12 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold underline bg-red-100 text-center">
+      Hello world!
+    </h1>
+  )
+}
+
+export default App
