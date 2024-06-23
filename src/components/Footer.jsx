8aee73f0eecb9abd42fcefb0041709bafac1f714
juanzb <juanzarache2000@gmail.com>
2024-06-23 18:04:56 -0500

    Mejorando Diseño General

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,17 +2,16 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-gray-400 py-8">
-      <div className="container mx-auto px-4">
+    <footer className="bg-gray-800 text-gray-400 py-8 ">
+      <div className="w-full flex justify-center">
         <div className="flex flex-wrap">
-          <div className="w-full md:w-1/3 text-center md:text-left mb-4 md:mb-0">
+          <div className="w-1/3 text-left">
             <h2 className="text-2xl font-bold text-gray-400">MOVIES</h2>
             <p className="mt-2">Find and explore your favorite movies and TV shows with our comprehensive database.</p>
           </div>
-          <div className="w-full md:w-1/3 flex justify-center mb-4 md:mb-0">
-            <div className="grid grid-cols-1 text-center">
+          <div className="w-1/3 flex justify-center mb-4 ">
+            <div className="grid grid-cols-1">
             <h2 className="text-2xl font-bold text-gray-400 uppercase">social networks</h2>
-
               <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300">
               Facebook
               </a>
@@ -24,20 +23,20 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div className="w-full md:w-1/3 text-center ">
-            <nav className=" grid grid-cols-1">
+          <div className="w-1/3 flex mb-4 justify-center">
+            <nav className=" grid grid-cols-1 ">
             <h2 className="text-2xl font-bold text-gray-400 uppercase">Navigation</h2>
-              <Link to="/top-rated" className="hover:text-yellow-300">Top Rated</Link>
-              <Link to="/now-playing" className="hover:text-yellow-300">Now Playing</Link>
-              <Link to="/popular" className="hover:text-yellow-300">Popular</Link>
-              <Link to="/upcoming" className="hover:text-yellow-300">Upcoming</Link>
+              <Link to="/TopRated" className="hover:text-yellow-300">Top Rated</Link>
+              <Link to="/NowPlay" className="hover:text-yellow-300">Now Playing</Link>
+              <Link to="/Popular" className="hover:text-yellow-300">Popular</Link>
+              <Link to="/Upcoming" className="hover:text-yellow-300">Upcoming</Link>
             </nav>
           </div>
         </div>
+      </div>
         <div className="border-t border-gray-700 mt-8 pt-4 text-center">
           <p>&copy; 2024 Movies. All rights reserved.</p>
         </div>
-      </div>
     </footer>
   )
     };
